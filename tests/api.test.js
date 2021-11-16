bbf6a781406745fe9dd4bf1be177a14c9bbb604b
Jyri Kemppainen <jyri.kemppainen@karelia.fi>
2021-11-16 18:45:31 +0200

    lll

--- tests/api.test.js
+++ tests/api.test.js
@@ -2,8 +2,8 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("Test the path api/places", () => {
-    test("Response to the GET", async () => {
-        const res = await request(app)
+    test("Response to the GET", () => {
+        return request(app)
             .get("/api/places")
             .expect(200); // http message 200 => the request succeeded
     });
